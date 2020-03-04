175c95d7cb1bc44c99ba34c92732ff3205b38fe2
Asher Kobin <Asher@Ashers-MBP.fios-router.home>
2020-03-03 23:24:21 -0500

    fix port assignment logic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const server = require("./api/server");
-const portNum = process.env.PORT || isDebug() ? 6061 : 6060;
+const portNum = (process.env.PORT) || (isDebug() ? 6061 : 6060);
 
 server.listen(portNum, () =>
   console.log(`** Express Running on ${portNum} **`)
