45f5dd9683429816079219386245134d4b2cb329
Asher Kobin <Asher@Ashers-MBP.fios-router.home>
2020-03-04 20:05:17 -0500

    public get auction/:id returns bids

--- api/public-router.js
+++ api/public-router.js
@@ -1,6 +1,7 @@
 const publicRouter = require("express").Router();
 const categoriesModel = require("../categories/categories-model");
 const auctionsModel = require("../auctions/auctions-model");
+const bidsModel = require("../bids/bids-model");
 
 publicRouter.get("/categories", async (req, res) => {
   const allCategories = await categoriesModel.getAll();
@@ -10,8 +11,26 @@ publicRouter.get("/categories", async (req, res) => {
 
 publicRouter.get("/auctions", async (req, res) => {
   const allAuctions = await auctionsModel.getAll();
+
+  // for (const auction of allAuctions) {
+  //   auction.bids = await bidsModel.getBidsByAuctionId(auction.id);
+  // }
   
   res.status(200).json(allAuctions);
+});
+
+publicRouter.get("/auctions/:id", async (req, res) => {
+  const allAuctions = await auctionsModel.getAuctionById(req.params.id);
+  const foundAuction = allAuctions[0];
+
+  if (!foundAuction) {
+    res.status(400).json({ errorMessage: "Auction not found"} );
+  }
+  else {
+    foundAuction.bids = await bidsModel.getBidsByAuctionId(foundAuction.id);
+    
+    res.status(200).json(foundAuction);
+  }
 });
 
 module.exports = publicRouter;
--- bids/bids-model.js
+++ bids/bids-model.js
@@ -4,6 +4,7 @@ const bidsSelect = [
   "bids.id as id",
   "auctions.id as auction_id",
   "bids.buyer_id as buyer_id",
+  "users.username as buyer_name",
   "bids.bid_amount as bid_amount",
   "bids.created_at as bid_date"
 ];
@@ -11,7 +12,8 @@ const bidsSelect = [
 function getAll() {
   return DB("bids")
     .select(...bidsSelect)
-    .join("auctions", "auctions.id", "bids.auction_id");
+    .join("auctions", "auctions.id", "bids.auction_id")
+    .join("users", "users.id", "bids.buyer_id");
 }
 
 function add(bidInfo) {
@@ -26,12 +28,22 @@ function getBidsByBuyerId(buyerId) {
   return DB("bids")
     .select(...bidsSelect)
     .join("auctions", "auctions.id", "bids.auction_id")
+    .join("users", "users.id", "bids.buyer_id")
     .where("bids.buyer_id", buyerId);
+}
+
+function getBidsByAuctionId(auctionId) {
+  return DB("bids")
+    .select(...bidsSelect)
+    .join("auctions", "auctions.id", "bids.auction_id")
+    .join("users", "users.id", "bids.buyer_id")
+    .where("bids.auction_id", auctionId);
 }
 
 module.exports = {
   getAll,
   getBidsByBuyerId,
+  getBidsByAuctionId,
   add,
   remove
 };
